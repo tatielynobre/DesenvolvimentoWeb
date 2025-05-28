413b76362c2de8513995cf2b82b5a94d27cb618c
tatiely <tatiely.lima.nobre@gmail.com>
2025-05-28 08:57:47 -0300

    estudos e ap1

---
+++ aulas/CallbackJS/script.js
@@ -0,0 +1,16 @@
+//Funções JS
+
+function soma(num1,num2){
+    return num1+num2;
+}
+
+
+//função anônima 
+const somar_arrow = (num1, num2)=>{
+    return num1+num2;
+}
+
+//função anônima 
+const somar_arrow_sem_corpo = (num1, num2) => num1+num2;
+
+console.log(somar_arrow_sem_corpo(1,2))
